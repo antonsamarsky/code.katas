b5cbdc197afabcb6eb0be932ed8a111837836268
asa <asa@e-conomic.com>
2012-02-12 21:49:30 +0200

    Complete first tdd kata: String Calculator: http://osherove.com/tdd-kata-1/
    Used doping: two bottles of beer.

---
+++ src/csharp/StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+	public class StringCalculator
+	{
+		const string DefaultDelimiter = ",";
+		const string SingleDelimiterRegexp = @"//(?<delimiter>\W+)\n";
+		const string MultipleDelimiterRegexp = @"//(?<delimiter>\W+)\n";
+
+		public int Add(string input)
+		{
+			if (input == null)
+			{
+				throw new ArgumentNullException("input");
+			}
+
+			if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+			{
+				return 0;
+			}
+
+			var delimiter = DefaultDelimiter;
+
+			var delimiterRegExp = new Regex(SingleDelimiterRegexp);
+			if (delimiterRegExp.IsMatch(input))
+			{
+				var multipleDelimiterRegexp = new Regex(MultipleDelimiterRegexp);
+				delimiter = multipleDelimiterRegexp.IsMatch(input) ? 
+					multipleDelimiterRegexp.Match(input).Result("${delimiter}") :
+					delimiterRegExp.Match(input).Result("${delimiter}");
+
+				input = delimiterRegExp.Replace(input, string.Empty);
+			}
+
+			var invalidDelimiter = string.Format("{0}\n", delimiter);
+			if (input.Contains(invalidDelimiter))
+			{
+				throw new InvalidOperationException("Invalid delimiter.");
+			}
+
+			var numbers = input
+				.Split(delimiter.ToArray())
+				.SelectMany(c => c.Split('\n'))
+				.Where(c => !string.IsNullOrEmpty(c))
+				.Select(c => c.Trim())
+				.Select(int.Parse).ToArray();
+
+			if (numbers.Any(n => n < 0))
+			{
+				var negativesValues = string.Join(", ", numbers.Where(n => n < 0).Select(n => n.ToString(CultureInfo.InvariantCulture)).ToArray());
+				throw new InvalidOperationException(string.Concat("There are negatives values: ", negativesValues));
+			}
+
+			return numbers.Where(n => n < 1000).Sum();
+		}
+	}
+}
---
+++ src/csharp/StringCalculator/StringCalculator/StringCalculatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+	[TestFixture]
+	public class StringCalculatorTest
+	{
+		[Test]
+		public void CtorTest()
+		{
+			var stringCalculator = new StringCalculator();
+			stringCalculator.Should().NotBeNull();
+		}
+
+		[TestCase(null, ExpectedException = typeof(ArgumentNullException))]
+		[TestCase("", Result = 0)]
+		[TestCase(" ", Result = 0)]
+		[TestCase("1", Result = 1)]
+		[TestCase("999", Result = 999)]
+		[TestCase("1,2", Result = 3)]
+		[TestCase("1\n2,3", Result = 6)]
+		[TestCase("1,\n", ExpectedException = typeof(InvalidOperationException))]
+		[TestCase("//;\n1;2", Result = 3)]
+		[TestCase("//;\n1;-2;5;-6", ExpectedException = typeof(InvalidOperationException), ExpectedMessage = "There are negatives values: -2, -6")]
+		[TestCase("//+\n2+1001", Result = 2)]
+		[TestCase("//[***]\n1***2***3", Result = 6)]
+		[TestCase("//[*][%]\n1*2%3", Result = 6)]
+		[TestCase("//[***][%]\n1***2%3", Result = 6)]
+		[TestCase("//[***][%]\n1***2%3***5", Result = 11)]
+		[TestCase("//[***][%][,]\n1***2%3,5", Result = 11)]
+		public int AddTest(string number)
+		{
+			var stringCalculator = new StringCalculator();
+			int result = stringCalculator.Add(number);
+
+			return result;
+		}
+	}
+}
